25735b5ac7b3d47f2cd9b651f6595669b80ada70
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-10-24 10:46:02 -0400

    changed dollar values to have a max of 2 decimal places & edited style of output

--- bank_account.py
+++ bank_account.py
@@ -10,28 +10,28 @@ class BankAccount:
     # class methods
     def deposit(self, amount):
         self.balance += amount 
-        print(f"Amount Deposited: ${amount} > New Balance: ${self.balance}")
+        print(f"Amount Deposited: ${amount:.2f} > New Balance: ${self.balance:.2f}")
 
     def withdraw(self, amount):
         amount += self.balance
         if amount > self.balance:
             self.balance -= 10
-            print(f"Insufficient funds. You have also been charged an overdraft fee of $10.\nYour new balance is: ${self.balance}")
+            print(f"Insufficient funds. You have also been charged an overdraft fee of $10.00\nYour new balance is: ${self.balance:.2f}")
         else: 
             self.balance -= amount
-            print(f"Amount Withdrawn: ${amount} > New Balance: ${self.balance}")
+            print(f"Amount Withdrawn: ${amount:.2f} > New Balance: ${self.balance:.2f}")
 
     def get_balance(self):
-        print(f"Your current account balance is ${self.balance}.")
+        print(f"Your current account balance is ${self.balance:.2f}.")
         return self.balance
     
     def add_interest(self):
         interest = self.balance * 0.00083
         self.balance += interest
-        print(f"You have gained an interest of ${interest}, bringing your total current balance to: ${self.balance}")
+        print(f"You have gained an interest of ${interest}, bringing your total current balance to: ${self.balance:.2f}") # should i cap the interest to 2 decimal places?
 
     def print_statement(self):
-        print(f"{self.full_name}\nAccount No.: {self.account_number}\nBalance: {self.balance}") # figure out a way to hide the first 4 account numbers
+        print(f"{self.full_name}\nAccount No.: {self.account_number}\nBalance: ${self.balance:.2f}") # figure out a way to hide the first 4 account numbers
 
 # 3 different bank accounts
 user_harry = BankAccount("Harry Potter")
@@ -39,20 +39,26 @@ user_harry.deposit(100)
 user_harry.withdraw(50)
 user_harry.get_balance()
 user_harry.add_interest()
+print("---")
 user_harry.print_statement()
+print("\n")
 
 user_ron = BankAccount("Ron Weasley")
 user_ron.deposit(150)
 user_ron.withdraw(200)
 user_ron.get_balance()
 user_ron.add_interest()
+print("---")
 user_ron.print_statement()
+print("\n")
 
 user_hermione = BankAccount("Hermione Granger")
 user_hermione.deposit(200)
 user_hermione.withdraw(100)
 user_hermione.get_balance()
 user_hermione.add_interest()
+print("---")
 user_hermione.print_statement()
+print("\n")
 
 # note: fix decimal places 
