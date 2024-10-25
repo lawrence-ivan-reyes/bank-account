41dc5e480ad953f70ab4707702870a1eb291b491
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-10-25 11:11:25 -0400

    added rounding to interest, fixed withdraw method, and added style changes

--- bank_account.py
+++ bank_account.py
@@ -13,10 +13,9 @@ class BankAccount:
         print(f"Amount Deposited: ${amount:.2f} > New Balance: ${self.balance:.2f}") # .2f across the entire project to have 2 decimal places across all dollar values in the output
 
     def withdraw(self, amount):
-        amount += self.balance
         if amount > self.balance:
             self.balance -= 10
-            print(f"Insufficient funds. You have also been charged an overdraft fee of $10.00\nYour new balance is: ${self.balance:.2f}")
+            print(f"Insufficient funds. You have also been charged an overdraft fee of $10.00.\nYour new balance is: ${self.balance:.2f}")
         else: 
             self.balance -= amount
             print(f"Amount Withdrawn: ${amount:.2f} > New Balance: ${self.balance:.2f}")
@@ -26,13 +25,13 @@ class BankAccount:
         return self.balance
     
     def add_interest(self):
-        interest = self.balance * 0.00083
+        interest = round(self.balance * 0.00083, 2) # added rounding to bring this number to 2 decimal places
         self.balance += interest
-        print(f"You have gained an interest of ${interest}, bringing your total current balance to: ${self.balance:.2f}") 
+        print(f"You have gained an interest of ${interest}, bringing your total current balance to: ${self.balance:.2f}.") 
 
     def print_statement(self):
         masked_account_number = "****" + str(self.account_number)[4:]
-        print(f"{self.full_name}\nAccount No.: {masked_account_number}\nBalance: ${self.balance:.2f}")
+        print(f"Account Details:\n{self.full_name}\nAccount No.: {masked_account_number}\nBalance: ${self.balance:.2f}")
 
 # 3 different bank accounts
 user_harry = BankAccount("Harry Potter")
@@ -40,24 +39,24 @@ user_harry.deposit(100)
 user_harry.withdraw(50)
 user_harry.get_balance()
 user_harry.add_interest()
-print("---")
-user_harry.print_statement()
 print("\n")
+user_harry.print_statement()
+print("\n---\n")
 
 user_ron = BankAccount("Ron Weasley")
 user_ron.deposit(150)
 user_ron.withdraw(200)
 user_ron.get_balance()
 user_ron.add_interest()
-print("---")
-user_ron.print_statement()
 print("\n")
+user_ron.print_statement()
+print("\n---\n")
 
 user_hermione = BankAccount("Hermione Granger")
 user_hermione.deposit(200)
 user_hermione.withdraw(100)
 user_hermione.get_balance()
 user_hermione.add_interest()
-print("---")
-user_hermione.print_statement()
 print("\n")
+user_hermione.print_statement()
+print("\n---\n")
