a702321438952067bf7a7f70888ec4ce2173ca60
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2024-10-25 12:30:40 -0400

    working through stretch challenge 4

--- bank.py
+++ bank.py
@@ -1,1 +1,12 @@
+from bank_account import BankAccount
+
 # stretch challenge 4
+class Bank:
+    def __init__(self):
+        self.accounts = []
+
+    def create_account(self, full_name, account_type = "checking"):
+        new_account = BankAccount(full_name, account_type)
+        self.accounts.append(new_account)
+        print(f"\nAccount created for {full_name} with account number {new_account.account_number}.")
+    
